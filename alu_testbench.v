73157390cc9058b8492f0fc50302b2aa6f57fc87
Enrique Macaya <enriquemacaya1@gmail.com>
2024-11-05 20:20:08 -0600

    ALU TB Updates

--- alu_testbench.v
+++ alu_testbench.v
@@ -13,11 +13,40 @@ module custom_alu_tb;
         .Z(Z),
     );
     
-initial begin
+    initial begin
     
-    $monitor("OP: %b | A: %b | B: %b | Z: %b", OP, A, B, Z);
+        $monitor("OP: %b | A: %b | B: %b | Z: %b", OP, A, B, Z);
 
-    OP = 4'b0000; A = 8'b0; B = 8'b0;
-    #10;
+        OP = 4'b0000; A = 8'b0; B = 8'b0;
+        #10;
 
-    
+        OP = 4'b0001;
+        #10;
+
+        OP = 4'b0010;
+        #10;
+
+        OP = 4'b0011;
+        #10;
+
+        OP = 4'b0100;
+        #10;
+
+        OP = 4'b0101;
+        #10;
+
+        OP = 4'b0110;
+        #10;
+
+        OP = 4'b0111;
+        #10;
+
+        OP = 4'b1000;
+        #10;
+
+        OP = 4'b1001;
+        #10;
+
+        $finish;
+    end
+endmodule
